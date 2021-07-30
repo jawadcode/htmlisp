1b9da9bde8822dc5f33be508c1ab4aaefba9ce1d
kizza7984 <54994771+kizza7984@users.noreply.github.com>
2021-07-30 22:29:07 +0100

    Add watch mode

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,11 @@ use std::{
     io::{self, Write},
     path::Path,
     process,
+    sync::mpsc::channel,
+    time::Duration,
+    path::PathBuf
 };
+use notify::{Watcher, RecursiveMode, DebouncedEvent, watcher};
 
 fn main() {
     let mut args = env::args();
@@ -97,12 +101,65 @@ fn watch(config: &Config) -> Result<(), ProgramError> {
         return Err(ProgramError::WatchDirIncorrect(config.watch.clone()));
     }
 
-    // // stuff
-    // loop {
-    //     // things
-    // }
+    let (transmit, receive) = channel();
+    let mut watcher = watcher(transmit, Duration::from_secs(1)).unwrap(); // Create watcher with 1 second debounce time
 
-    Ok(())
+    watcher.watch(&config.watch, RecursiveMode::Recursive).unwrap(); // Watch for file system events in all subfolders of the specified directory
+    println!("\x1b[94;1mInfo:\x1b[0m Watching for changes in {}...", &config.watch);
+
+    loop {
+        match receive.recv() {
+            Ok(event) => match event {
+                DebouncedEvent::Write(changed_file_path) => { // Match if the event is a file write event
+                    let extension = changed_file_path.extension();
+                    match extension {
+                        Some(extension) => { // Match if the file has an extension
+                            if extension == "htmlisp" {
+
+                                // Construct output path
+                                let watch_dir = PathBuf::from(&config.watch);
+                                let mut output_path = PathBuf::from("output/");
+
+                                let absolute_watch_dir = watch_dir.canonicalize().map_err(|e| ProgramError::ReadInput(e))?;
+                                let absolute_changed_file_path = changed_file_path.canonicalize().map_err(|e| ProgramError::ReadInput(e))?;
+                                let relative_changed_file_path = absolute_changed_file_path.strip_prefix(absolute_watch_dir).expect("Couldn't calculate output path");
+
+                                output_path.push(relative_changed_file_path); // (Relative means relative to the watch directory)
+                                output_path.set_extension("html");
+
+                                // Create new config
+                                match Config::new(&mut env::args()) {
+                                    Ok(config) => {
+                                        let mut config = config; // Make value from match mutable
+                                        config.input_file = changed_file_path.to_str().unwrap().to_string();
+                                        config.output_file = output_path.to_str().unwrap().to_string();
+                                        println!("\x1b[94;1mInfo:\x1b[0m Re-compiling due to changes...");
+
+                                        // Parse changed file with new config
+                                        match read_write(&config) {
+                                            Ok(()) => println!("\x1b[32;1mSuccess:\x1b[0m {} -> {}", relative_changed_file_path.to_string_lossy(), &config.output_file),
+                                            // Handle error here instead of propagating it so that the loop keeps running
+                                            Err(err) => eprintln!("\x1b[31;1mError:\x1b[0m {}: {}", err, relative_changed_file_path.to_string_lossy())
+                                        }
+                                    },
+                                    Err(err) => {
+                                        eprintln!("\x1b[31;1mError:\x1b[0m {}", err);
+                                        process::exit(1);
+                                    }
+                                }
+                            }
+                        },
+                        None => {} // Do nothing if the file doesn't have an extension
+                    }
+                },
+                _ => {} // Do nothing for all other file events
+            },
+            Err(err) => {
+                eprintln!("\x1b[31;1mError:\x1b[0m Watch error: {:?}", err);
+                process::exit(1);
+            },
+        }
+    }
 }
 
 fn help() {
@@ -118,6 +175,9 @@ Usage:
     
     Optional Flags:
         -p/--prettify Output prettified HTML
+        -w/--watch <directory> Watch a directory for changes and re-compile
+        Outputs to <working directory>/output/, preserving input directory structure
+        Makes -i and -o optional
 
 
 Note:
